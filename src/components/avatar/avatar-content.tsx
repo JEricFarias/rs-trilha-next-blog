6bffb478038005d28dbeb9eb722d397d5c59bf0e
JEricFarias <ericfarrias@hotmail.com>
2025-06-24 22:10:20 -0300

    refactor: apply compound pattern to avatar component

---
+++ src/components/avatar/avatar-content.tsx
@@ -0,0 +1,5 @@
+type AvatarContentProps = { children: React.ReactNode };
+
+export function AvatarContent({ children }: AvatarContentProps) {
+  return <div className="flex flex-col">{children}</div>;
+}
